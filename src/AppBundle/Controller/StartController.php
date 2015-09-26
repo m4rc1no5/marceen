2551309e09944a97c049123020bea343cd1098de
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-09-26 09:55:00 +0200

    Start page

--- src/AppBundle/Controller/StartController.php
+++ src/AppBundle/Controller/StartController.php
@@ -2,9 +2,18 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class StartController extends Controller
 {
-
+    /**
+     * @Route("/", name="start")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
 }
