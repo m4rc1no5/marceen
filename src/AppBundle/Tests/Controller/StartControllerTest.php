fa72b896953756d0758cc44b9766880e06939fc5
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-09-30 20:28:01 +0200

    Testy

--- src/AppBundle/Tests/Controller/StartControllerTest.php
+++ src/AppBundle/Tests/Controller/StartControllerTest.php
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Programowanie', $crawler->filter('#main_content h1')->text());
     }
 }
