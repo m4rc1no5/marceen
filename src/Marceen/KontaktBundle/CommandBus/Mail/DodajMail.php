e65e9e1ad07d49c6204ec55130dbd251ae9d7611
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-10-17 18:16:55 +0200

    Command - add mail

---
+++ src/Marceen/KontaktBundle/CommandBus/Mail/DodajMail.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcinos
+ * Date: 17.10.15
+ * Time: 18:10
+ */
+
+namespace Marceen\KontaktBundle\CommandBus\Mail;
+
+
+use AppBundle\CommandBus\Mail\MailCommand;
+
+class DodajMail extends MailCommand
+{
+
+    /** @var string */
+    protected $email_to;
+
+    /**
+     * @param string $email_to
+     */
+    public function __construct($email_to)
+    {
+        $this->email_to = $email_to;
+    }
+
+    public static function messageName()
+    {
+        return 'mail.dodaj';
+    }
+
+}
--- src/Marceen/KontaktBundle/CommandBus/Mail/MailCommand.php
+++ src/Marceen/KontaktBundle/CommandBus/Mail/MailCommand.php
@@ -8,10 +8,46 @@
 
 namespace AppBundle\CommandBus\Mail;
 
-
 use AppBundle\CommandBus\Command;
+use Symfony\Component\Validator\Constraints as Assert;
 
 abstract class MailCommand extends Command
 {
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(max="128")
+     */
+    protected $name;
 
+    /**
+     * @var string
+     *
+     * @Assert\Email()
+     * @Assert\Length(max="128")
+     */
+    protected $email_from;
+
+    /**
+     * @var string
+     *
+     * @Assert\Email()
+     * @Assert\Length(max="128")
+     */
+    protected $email_to;
+
+    /**
+     * @var string
+     *
+     * @Assert\Length(max="32")
+     */
+    protected $phone;
+
+    /**
+     * @var string
+     *
+     * @Assert\Length(max="2048")
+     */
+    protected $message;
 }
